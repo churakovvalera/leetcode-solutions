1ef03b123888d9e62490d96b82fa98a686b6e737
churakovvalera <churakovvaler@yandex.ru>
2024-06-23 10:29:14 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,10 +9,8 @@ class Solution(object):
                 return [num_map[complement], i]
             num_map[num] = i
 
-
-# Примеры использования:
 sol = Solution()
-print(sol.twoSum([2, 7, 11, 15], 9))  # Вывод: [0, 1]
-print(sol.twoSum([3, 2, 4], 6))       # Вывод: [1, 2]
+print(sol.twoSum([2, 7, 11, 15], 9))
+print(sol.twoSum([3, 2, 4], 6))
 print(sol.twoSum([3, 3], 6))
 print(sol.twoSum([3,5,9,7],12))
